ae7ebaea71440a6bd1365a4860852a2467f4a399
Tommaullin <Tommaullin@gmail.com>
2021-03-01 13:42:53 +0000

    wip

--- TwoSmp/join_and_plot_2mu.py
+++ TwoSmp/join_and_plot_2mu.py
@@ -26,7 +26,7 @@ def joinAndPlot(OutDir, simNo):
     # center and, as usual, the number of subjects.
     #
     # ==========================================================================
-    if simNo in [1, 2, 3, 4]:
+    if simNo in [1, 2, 3, 4, 5, 6, 7, 8]:
 
         # Variable to check if this is the first file weve looked at
         first = True
