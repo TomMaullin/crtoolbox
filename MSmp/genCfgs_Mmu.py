08e4252ad1a7a399a4c73f42bdd2990910a84440
Tommaullin <Tommaullin@gmail.com>
2021-07-06 23:47:13 +0100

    wip

--- MSmp/genCfgs_Mmu.py
+++ MSmp/genCfgs_Mmu.py
@@ -2,6 +2,40 @@ import os
 import numpy as np
 import yaml
 
+# Powerset function, taken from:
+# https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
+def powerset(s):
+
+    # Get length of x
+    x = len(s)
+
+    # Masks is a list of `aliases' for the elements of s. I.e. for the
+    # i^th element of s, the corresponding element of masks is 2^(i+1).
+    masks = [1 << i for i in range(x)]
+    
+    # a << x is the same as a*(2^x) so we are looping over length of
+    # powerset here
+    for i in range(1,1 << x):
+
+        # Yeild makes a generator obect
+        # zip(masks,s) is pairing each element of s with a power of 2
+        yield np.array([ss for mask, ss in zip(masks, s) if i & mask])
+
+m = 5
+alphas=list(powerset(np.arange(m)))
+
+
+# Even circle points function, taken from:
+# https://stackoverflow.com/questions/33510979/generator-of-evenly-spaced-points-in-a-circle-in-python
+def circle_points(r, n):
+    circles = []
+    for r, n in zip(r, n):
+        t = np.linspace(0, 2*np.pi, n, endpoint=False)
+        x = np.round(r * np.cos(t))
+        y = np.round(r * np.sin(t))
+        circles.append(np.c_[x, y])
+    return circles[0]
+    
 def generateCfgs(OutDir, simNo):
 
     # Make simulation directory
