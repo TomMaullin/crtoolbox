8be87e6eaf635d2df701254d4f58d6ae53721b46
Tommaullin <Tommaullin@gmail.com>
2021-07-16 16:51:29 +0100

    added sim2

--- MSmp/genCfgs_Mmu.py
+++ MSmp/genCfgs_Mmu.py
@@ -86,7 +86,7 @@ def generateCfgs(OutDir, simNo):
     inputs['nBoot'] = 5000
 
     # Number of fields, m
-    ms = np.arange(2,8)
+    ms = np.arange(2,6)
 
     # This determines how close together the circles are.
     # (spacing r is the distance from each circle center to the
@@ -97,103 +97,223 @@ def generateCfgs(OutDir, simNo):
     nSubs = np.linspace(40,500,24)
     fg_nSubs = np.array([100,300,500]) 
 
-    # Add threshold c
-    inputs['c'] = 2
-
     # Add mode
     inputs['mode'] = 3
 
-    # Id for config file
-    cfgId = 1
+    # ==========================================================================
+    #
+    # Simulation 1: Circles in a circle
+    #
+    # --------------------------------------------------------------------------
+    #
+    # In this simulation setting, we are interested in m circles equally spread 
+    # in a circle, as we vary m
+    #
+    # ==========================================================================
+    if simNo==1:
+    
+        # Add threshold c
+        inputs['c'] = 2
 
-    # Loop through all m values
-    for m in ms:
+        # Id for config file
+        cfgId = 1
 
-        # Add m
-        inputs['m']=int(m)
+        # Loop through all m values
+        for m in ms:
 
-        # ---------------------------------------------------------------
-        # Mus
-        # ---------------------------------------------------------------
-        # Create empty specifications
-        mus = {}
+            # Add m
+            inputs['m']=int(m)
 
-        # Loop through mus, adding each field in turn
-        for i in np.arange(m):
+            # ---------------------------------------------------------------
+            # Mus
+            # ---------------------------------------------------------------
+            # Create empty specifications
+            mus = {}
 
-            # New empty dict
-            mus['mu'+str(i+1)]={}
+            # Loop through mus, adding each field in turn
+            for i in np.arange(m):
 
-            # Mu type
-            mus['mu'+str(i+1)]['type'] = 'circle2D' 
+                # New empty dict
+                mus['mu'+str(i+1)]={}
 
-            # Mu FWHM
-            mus['mu'+str(i+1)]['fwhm'] = 'np.array([5,5])'
+                # Mu type
+                mus['mu'+str(i+1)]['type'] = 'circle2D' 
 
-            # Mu r
-            mus['mu'+str(i+1)]['r'] = 40
+                # Mu FWHM
+                mus['mu'+str(i+1)]['fwhm'] = 'np.array([5,5])'
 
-            # Mu magnitude
-            mus['mu'+str(i+1)]['mag'] = 3
+                # Mu r
+                mus['mu'+str(i+1)]['r'] = 40
 
-            # Get some evenly spaced center points
-            centers = circle_points(np.array([spacing_r]),np.array([m]))
+                # Mu magnitude
+                mus['mu'+str(i+1)]['mag'] = 3
 
-            # Mu center
-            mus['mu'+str(i+1)]['center'] = 'np.'+repr(centers[i,:].astype(np.int))
+                # Get some evenly spaced center points
+                centers = circle_points(np.array([spacing_r]),np.array([m]))
 
-        # ---------------------------------------------------------------
-        # Epsilons
-        # ---------------------------------------------------------------
-        # Create empty specifications
-        noises = {}
+                # Mu center
+                mus['mu'+str(i+1)]['center'] = 'np.'+repr(centers[i,:].astype(np.int))
 
-        # Loop through noises, adding each field in turn
-        for i in np.arange(m):
+            # ---------------------------------------------------------------
+            # Epsilons
+            # ---------------------------------------------------------------
+            # Create empty specifications
+            noises = {}
 
-            # New empty dict
-            noises['noise'+str(i+1)]={}
+            # Loop through noises, adding each field in turn
+            for i in np.arange(m):
 
-            # Add FWHM
-            noises['noise'+str(i+1)]['FWHM'] = '[0, 3, 3]'
+                # New empty dict
+                noises['noise'+str(i+1)]={}
 
-            # Add type
-            noises['noise'+str(i+1)]['type'] = 'homogen'
+                # Add FWHM
+                noises['noise'+str(i+1)]['FWHM'] = '[0, 3, 3]'
 
-        # Save mus and noises
-        inputs['mus']=mus
-        inputs['noises']=noises
+                # Add type
+                noises['noise'+str(i+1)]['type'] = 'homogen'
 
-        # Loop through all nSub settings
-        for nSub in nSubs:
+            # Save mus and noises
+            inputs['mus']=mus
+            inputs['noises']=noises
 
-            # Add nSub to inputs
-            inputs['nSub'] = int(nSub)
+            # Loop through all nSub settings
+            for nSub in nSubs:
 
-            # Save cfg ID (handy to have around)
-            inputs['cfgId'] = int(cfgId)
+                # Add nSub to inputs
+                inputs['nSub'] = int(nSub)
 
-            # Record if we want to save figures for this design or not
-            if (nSub in fg_nSubs):
+                # Save cfg ID (handy to have around)
+                inputs['cfgId'] = int(cfgId)
 
-                # In this case we do want to save  figures
-                inputs['figGen']=1
+                # Record if we want to save figures for this design or not
+                if (nSub in fg_nSubs):
 
-            else:
+                    # In this case we do want to save  figures
+                    inputs['figGen']=1
 
-                # In this case we do want to save  figures
-                inputs['figGen']=0
+                else:
 
-            # Save the yml
-            with open(os.path.join(simDir,'cfgs','cfg'+str(cfgId)+'.yml'), 'w') as outfile:
-                yaml.dump(inputs, outfile, default_flow_style=False)
+                    # In this case we do want to save  figures
+                    inputs['figGen']=0
 
-            # Incremement cfgID
-            cfgId = cfgId + 1
+                # Save the yml
+                with open(os.path.join(simDir,'cfgs','cfg'+str(cfgId)+'.yml'), 'w') as outfile:
+                    yaml.dump(inputs, outfile, default_flow_style=False)
+
+                # Incremement cfgID
+                cfgId = cfgId + 1
 
 
-    # Delete fields which vary across simulation
-    del inputs['mus'], inputs['noises'], inputs['cfgId'], inputs['nSub']
+        # Delete fields which vary across simulation
+        del inputs['mus'], inputs['noises'], inputs['cfgId'], inputs['nSub']
+
+    # ==========================================================================
+    #
+    # Simulation 2: Circles in a circle (lower FWHM)
+    #
+    # --------------------------------------------------------------------------
+    #
+    # In this simulation setting, we are interested in m circles equally spread 
+    # in a circle, as we vary m
+    #
+    # ==========================================================================
+    if simNo==2:
+    
+        # Add threshold c
+        inputs['c'] = 2/3
+        
+        # Id for config file
+        cfgId = 1
+
+        # Loop through all m values
+        for m in ms:
+
+            # Add m
+            inputs['m']=int(m)
+
+            # ---------------------------------------------------------------
+            # Mus
+            # ---------------------------------------------------------------
+            # Create empty specifications
+            mus = {}
+
+            # Loop through mus, adding each field in turn
+            for i in np.arange(m):
+
+                # New empty dict
+                mus['mu'+str(i+1)]={}
+
+                # Mu type
+                mus['mu'+str(i+1)]['type'] = 'circle2D' 
+
+                # Mu FWHM
+                mus['mu'+str(i+1)]['fwhm'] = 'np.array([5,5])'
+
+                # Mu r
+                mus['mu'+str(i+1)]['r'] = 40
+
+                # Mu magnitude
+                mus['mu'+str(i+1)]['mag'] = 1
+
+                # Get some evenly spaced center points
+                centers = circle_points(np.array([spacing_r]),np.array([m]))
+
+                # Mu center
+                mus['mu'+str(i+1)]['center'] = 'np.'+repr(centers[i,:].astype(np.int))
+
+            # ---------------------------------------------------------------
+            # Epsilons
+            # ---------------------------------------------------------------
+            # Create empty specifications
+            noises = {}
+
+            # Loop through noises, adding each field in turn
+            for i in np.arange(m):
+
+                # New empty dict
+                noises['noise'+str(i+1)]={}
+
+                # Add FWHM
+                noises['noise'+str(i+1)]['FWHM'] = '[0, 3, 3]'
+
+                # Add type
+                noises['noise'+str(i+1)]['type'] = 'homogen'
+
+            # Save mus and noises
+            inputs['mus']=mus
+            inputs['noises']=noises
+
+            # Loop through all nSub settings
+            for nSub in nSubs:
+
+                # Add nSub to inputs
+                inputs['nSub'] = int(nSub)
+
+                # Save cfg ID (handy to have around)
+                inputs['cfgId'] = int(cfgId)
+
+                # Record if we want to save figures for this design or not
+                if (nSub in fg_nSubs):
+
+                    # In this case we do want to save  figures
+                    inputs['figGen']=1
+
+                else:
+
+                    # In this case we do want to save  figures
+                    inputs['figGen']=0
+
+                # Save the yml
+                with open(os.path.join(simDir,'cfgs','cfg'+str(cfgId)+'.yml'), 'w') as outfile:
+                    yaml.dump(inputs, outfile, default_flow_style=False)
+
+                # Incremement cfgID
+                cfgId = cfgId + 1
+
+
+        # Delete fields which vary across simulation
+        del inputs['mus'], inputs['noises'], inputs['cfgId'], inputs['nSub']
+
 
     #--------------------------------------------------------------------------------------
     # Save the baseline configuration (Note: This must be the last file output as the bash
