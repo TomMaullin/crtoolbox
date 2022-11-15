5898903d7ef5c9ff96040b4ac3e4fda7436a550b
TomMaullin <tommaullin@gmail.com>
2022-11-15 22:44:17 +0000

    functioning out CR generation

--- src/generate_CRs.py
+++ src/generate_CRs.py
@@ -40,7 +40,7 @@ import matplotlib.pyplot as plt
 def generate_CRs(data, c, p, out_dir, mask=None, n_boot=5000, tau='1/np.sqrt(nSub)'):
 
     # Evaulate tau
-    tau = eval(inputs['tau'])
+    tau = eval(tau)
 
     # Work out m, the number of samples we are considering
     m = data.shape[0]
