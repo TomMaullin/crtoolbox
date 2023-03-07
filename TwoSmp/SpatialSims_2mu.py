3bc0475268041b9f1ae7b90a6a61fc74360157a3
Tommaullin <Tommaullin@gmail.com>
2023-03-07 21:41:34 +0000

    improved printing for debugging

--- TwoSmp/SpatialSims_2mu.py
+++ TwoSmp/SpatialSims_2mu.py
@@ -2305,9 +2305,10 @@ def SpatialSims_2mu_seperate(ipath):
         estBdry_success_intrp[r,:] = (np.all(bdry_lowerCheck_estBdry_cap1 & bdry_lowerCheck_estBdry_cap2,axis=(1))) & \
                                      (np.all(bdry_upperCheck_estBdry_cap1 | bdry_upperCheck_estBdry_cap2,axis=(1)))
 
-        with open(os.path.join(simDir, 'RawResults','print.txt'), 'w') as f:
-            print('cap check shapes: ', bdry_lowerCheck_trueBdry_cap1.shape, bdry_lowerCheck_trueBdry_cap2.shape, bdry_upperCheck_trueBdry_cap1.shape, bdry_upperCheck_trueBdry_cap2.shape,file=f)
-            print('cap checks: ', bdry_lowerCheck_trueBdry_cap1, bdry_lowerCheck_trueBdry_cap2, bdry_upperCheck_trueBdry_cap1, bdry_upperCheck_trueBdry_cap2,file=f)
+        if inputs['mu2']['center']==inputs['mu1']['center']:
+            with open(os.path.join(simDir, 'RawResults','print.txt'), 'w+') as f:
+                print('cap check shapes: ', bdry_lowerCheck_trueBdry_cap1.shape, bdry_lowerCheck_trueBdry_cap2.shape, bdry_upperCheck_trueBdry_cap1.shape, bdry_upperCheck_trueBdry_cap2.shape,file=f)
+                print('cap checks: ', bdry_lowerCheck_trueBdry_cap1, bdry_lowerCheck_trueBdry_cap2, bdry_upperCheck_trueBdry_cap1, bdry_upperCheck_trueBdry_cap2,file=f)
 
         # -------------------------------------------------------------------
         # Check whether there were any boundary violations using interpolated
@@ -2358,13 +2359,14 @@ def SpatialSims_2mu_seperate(ipath):
         estBdry_success_intrp1[r,:] = np.all(bdry_lowerCheck_estBdry1,axis=(1)) & np.all(bdry_upperCheck_estBdry1,axis=(1)) # : AXES WONT WORK FOR 3D ATM
         estBdry_success_intrp2[r,:] = np.all(bdry_lowerCheck_estBdry2,axis=(1)) & np.all(bdry_upperCheck_estBdry2,axis=(1)) # : AXES WONT WORK FOR 3D ATM
 
-        with open(os.path.join(simDir, 'RawResults','print.txt'), 'w') as f:
-            print('noncap check shapes: ', bdry_lowerCheck_trueBdry1.shape, bdry_lowerCheck_trueBdry2.shape, bdry_upperCheck_trueBdry1.shape, bdry_upperCheck_trueBdry2.shape, file=f)
-            print('noncap checks: ', bdry_lowerCheck_trueBdry1, bdry_lowerCheck_trueBdry2, bdry_upperCheck_trueBdry1, bdry_upperCheck_trueBdry2, file=f)
+        if inputs['mu2']['center']==inputs['mu1']['center']:
+            with open(os.path.join(simDir, 'RawResults','print.txt'), 'w+') as f:
+                print('noncap check shapes: ', bdry_lowerCheck_trueBdry1.shape, bdry_lowerCheck_trueBdry2.shape, bdry_upperCheck_trueBdry1.shape, bdry_upperCheck_trueBdry2.shape, file=f)
+                print('noncap checks: ', bdry_lowerCheck_trueBdry1, bdry_lowerCheck_trueBdry2, bdry_upperCheck_trueBdry1, bdry_upperCheck_trueBdry2, file=f)
 
     if inputs['mu2']['center']==inputs['mu1']['center']:
 
-        with open(os.path.join(simDir, 'RawResults','print.txt'), 'w') as f:
+        with open(os.path.join(simDir, 'RawResults','print.txt'), 'w+') as f:
             print('trueBdry_success1', trueBdry_success1, file=f)
             print('trueBdry_success2', trueBdry_success2, file=f)
             print('trueBdry_success', trueBdry_success, file=f)
