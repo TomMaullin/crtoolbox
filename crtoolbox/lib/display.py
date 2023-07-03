ab1169ef87657abe159eb1f99840f3a3c1d4c32e
Tommaullin <Tommaullin@gmail.com>
2023-07-03 15:56:09 +0100

    added requirements.txt

--- crtoolbox/lib/display.py
+++ crtoolbox/lib/display.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 from lib.fileio import read_image
-from skimage import io
 import plotly.graph_objects as go
 import nibabel as nib
 import matplotlib.pyplot as plt
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+# Read in the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="crtoolbox",
-    version="0.1",
+    version="0.1.2",
     author="Tom Maullin",
     author_email="TomMaullin@gmail.com",
     description="The Confidence Regions Toolbox",
@@ -18,5 +22,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=requirements, 
     python_requires='>=3.6',
 )
