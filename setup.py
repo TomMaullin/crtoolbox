d09527b462c053ac3031cdabf6e7ecb139d83153
TomMaullin <tommaullin@gmail.com>
2023-07-03 15:08:34 +0100

    repo restructure for pip install

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crtoolbox",
+    version="0.1",
+    author="Tom Maullin",
+    author_email="TomMaullin@gmail.com",
+    description="The Confidence Regions Toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tommaullin/crtoolbox",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
